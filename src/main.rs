08868be67bfb35d386a2e9893145372e486250fc
Jacob Schroeder <j.alexander.sch@gmail.com>
2024-01-03 00:38:28 -0500

    serde json start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
+use serde::{Deserialize, Serialize};
+
+const TODO_PATH: &str = "./to.do";
 
 fn done_char(done: bool) -> char {
     if done == true {
@@ -7,6 +13,7 @@ fn done_char(done: bool) -> char {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Todo {
     id: usize,
     msg: String,
@@ -17,17 +24,42 @@ impl Todo {
     fn list(&mut self) {
         println!("{} - [{}] {}", self.id, done_char(self.done), self.msg);
     }
+
+    fn init() {
+        if Path::new(TODO_PATH).exists() {
+            println!("Whoops, to.do file already exists");
+        } else {
+            File::create(TODO_PATH).expect("Uh oh, to.do file creation failed");
+            println!("Created to.do file");
+        }
+    }
+
+    fn add(item: Todo) {
+        if !Path::new(TODO_PATH).exists() {
+            println!("Please initialize a to.do file with 'td init'");
+        } else {
+            let mut data = OpenOptions::new()
+                .append(true)
+                .open(TODO_PATH)
+                .expect("Cannot open to.do file");
+
+            let sj = serde_json::to_string(&item).unwrap();
+
+            data.write(sj.as_bytes()).expect("Failed to write to to.do file");
+        }
+    }
 }
 
 fn main() {
-    let item1: Todo = Todo{ id: 1, msg: "Test item 1".to_string(), done: true};
-    let item2: Todo = Todo{ id: 2, msg: "Test item 2".to_string(), done: true};
-    let item3: Todo = Todo{ id: 2, msg: "Read to.do file".to_string(), done: false};
-    let mut todos: Vec<Todo> = Vec::new();
-    todos.insert(0, item1);
-    todos.insert(1, item2);
-    todos.insert(2, item3);
-    for i in 0..todos.len() {
-        Todo::list(&mut todos[i]);
-    }
+    Todo::init();
+
+    let item1: Todo = Todo{ id: 1, msg: "Test item 1".to_owned(), done: true};
+    let item2: Todo = Todo{ id: 2, msg: "Test item 2".to_owned(), done: true};
+    let item3: Todo = Todo{ id: 2, msg: "Read to.do file".to_owned(), done: false};
+
+
+    Todo::add(item1);
+    Todo::add(item2);
+    Todo::add(item3);
+
 }
